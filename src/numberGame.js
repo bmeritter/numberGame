56706b5471d779d3d2948c339ab4e7f3f1aefcbb
bmeritter <bme_ritter@foxmail.com>
2016-10-05 10:59:45 +0800

    refactor return string

--- src/numberGame.js
+++ src/numberGame.js
@@ -2,18 +2,25 @@
  * Created by ritter on 16-10-5.
  */
 
+let numbers = {
+  '3': 'fizz',
+  '5': 'buzz',
+  '7': 'whizz'
+};
+
 function numberReport(number) {
   let result = '';
 
-  if (number % 3 === 0) {
-    result += 'fizz';
+  if (number % 3 === 0 ) {
+    result += numbers[3];
   }
   if (number % 5 === 0) {
-    result += 'buzz';
+    result += numbers[5];
   }
   if (number % 7 === 0) {
-    result += 'whizz';
+    result += numbers[7];
   }
+
   if (result === '') {
     result += number.toString();
   }
