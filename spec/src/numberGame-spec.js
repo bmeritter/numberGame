c93058b29b22d1f3ed2ca05faa44d88644f2c5ab
bmeritter <bme_ritter@foxmail.com>
2016-10-05 10:28:30 +0800

    init & test workspace

---
+++ spec/src/numberGame-spec.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ritter on 16-10-5.
+ */
+
+const numberReport = require('../../src/numberGame');
+
+describe('number game', ()=>{
+  it('', ()=>{
+    expect(1).toEqual(1);
+  });
+});
---
+++ src/numberGame.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ritter on 16-10-5.
+ */
+
+function numberReport(number) {
+  return 1;
+}
+
+module.exports = numberReport;
