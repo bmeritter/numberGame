657f7638d840aef375e548a6ae5d70c20623cb2d
bmeritter <bme_ritter@foxmail.com>
2016-10-05 10:53:58 +0800

    four spec 3, 5, 7, 21

--- spec/src/numberGame-spec.js
+++ spec/src/numberGame-spec.js
@@ -5,7 +5,30 @@
 const numberReport = require('../../src/numberGame');
 
 describe('number game', ()=>{
-  it('', ()=>{
-    expect(1).toEqual(1);
+
+  it('should return 1 when input 1', ()=>{
+    let expected = numberReport(1);
+    expect(expected).toEqual('1');
   });
+
+  it('should return fizz when input 3', ()=>{
+    let expected = numberReport(3);
+    expect(expected).toEqual('fizz');
+  });
+
+  it('should return buzz when input 5', ()=>{
+    let expected = numberReport(5);
+    expect(expected).toEqual('buzz');
+  });
+
+  it('should return whizz when input 7', ()=>{
+    let expected = numberReport(7);
+    expect(expected).toEqual('whizz');
+  });
+
+  it('should return fizzwhizz when input 21', ()=>{
+    let expected = numberReport(21);
+    expect(expected).toEqual('fizzwhizz');
+  });
+
 });
--- src/numberGame.js
+++ src/numberGame.js
@@ -3,7 +3,21 @@
  */
 
 function numberReport(number) {
-  return 1;
+  let result = '';
+
+  if (number % 3 === 0) {
+    result += 'fizz';
+  }
+  if (number % 5 === 0) {
+    result += 'buzz';
+  }
+  if (number % 7 === 0) {
+    result += 'whizz';
+  }
+  if (result === '') {
+    result += number.toString();
+  }
+  return result;
 }
 
 module.exports = numberReport;
